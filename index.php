2b274e29f77dae5048f860d7ccdb9e71a7ad4166
Mathieu MENUT <bardyl@ubuntu.(none)>
2014-02-11 18:10:24 +0100

    initialization

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index du site</title>
+	<link rel="stylesheet" href="css/flat-ui.css" />
+	<style>
+		body { background:#282828;padding-top:150px;}
+		h1 { color:#fff;font-weight: 300;}
+		#block { width:600px; margin:auto;}
+		input { width:500px;margin-top:20px;}
+		input[type="submit"]{margin-top:40px;margin-left:10px;cursor:pointer;}
+
+	</style>
+</head>
+<body>
+	<div id="block">
+		<h1>Home</h1>
+		<p class="btn btn-danger">Vous n'êtes pas connecté :(</p>
+		<p class="btn btn-success">Vous êtes connecté :)</p>
+
+		<p><a href="login.php">Connexion</a> - <a href="register.php">Inscription</a></p>
+	</div>
+</body>
+</html>
